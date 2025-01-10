7d8c467afbc5913b5612832ef2be1644162e67b5
Jan Koczot <janko@Jans-MacBook-Air.local>
2025-01-10 11:53:14 +0100

    Add tree.js file

---
+++ tree.js
@@ -0,0 +1,47 @@
+const leaves = "#";
+const count = 8;
+const rows = [];
+const trunk = "| |";
+let inverted = false; 
+let trunkOn = true;
+
+function padRow(rowNumber, rowCount) {
+    return " ".repeat(rowCount - rowNumber) + leaves.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+  }
+
+  for (let i = 1; i <= count; i++) {
+  if (inverted){
+    rows.unshift(padRow(i, count));
+  } else{
+    rows.push(padRow(i, count));
+}}
+
+//ALSO WORKING:
+
+    /*while(rows.length < count){
+        if (inverted){
+            rows.unshift(padRow(rows.length + 1, count));
+    }
+    else {
+        rows.push(padRow(rows.length + 1, count));
+    }};*/
+
+  if (trunkOn){
+    function trunkRow(){
+    return ".".repeat(count - 2) + trunk + ".".repeat(count - 2);
+    }}else{
+        function trunkRow(){
+            return " ".repeat(count - 2) + " ".repeat(2) + " ".repeat(count - 2);
+        }
+    }
+    
+
+  let result = "";
+  
+  for (const row of rows) {
+    result = result + row + "\n";
+  }
+
+console.log(result + trunkRow());
+
+  
