8c2881c013ee41df6c2ad61de9034a5cf1045c2f
JKoczot <koczot.j@gmail.com>
2025-01-31 13:37:48 +0100

    Patch 0.0.24

--- playwrightTestExercises/editFix.spec.ts
+++ playwrightTestExercises/editFix.spec.ts
@@ -1,11 +1,14 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('click edit button next to Phaedrum8', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/challenging_dom');
+    const text = "Phaedrum8";
 
-    const row = await page.getByText('Phaedrum8').locator('..');   
+    const row = await page.getByText(`${text}`).locator('..');   
 
     const editButton = row.getByRole('link', { name: 'edit' });
 
     await editButton.click();
+
+    await expect(page).toHaveURL('https://the-internet.herokuapp.com/challenging_dom#edit')
 });
